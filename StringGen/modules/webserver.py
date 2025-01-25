6939569943575a27804fefc6a08a4d623c71b9a1
shirokuo <shironihbos1@gmail.com>
2025-01-25 17:03:25 +0700

    Update webserver.py

--- StringGen/modules/webserver.py
+++ StringGen/modules/webserver.py
@@ -11,4 +11,4 @@ async def start_web_server():
     await runner.setup()
     site = web.TCPSite(runner, "0.0.0.0", 8000)  # Koyeb butuh port 8000
     await site.start()
-    print("Web server berjalan di port 8000"
+    print("Web server berjalan di port 8000")
